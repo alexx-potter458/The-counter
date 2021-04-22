78bc7fa5f60dab309cb60058e8a85d59b993456f
Alexandru Olaru <alexx.potter458@gmail.com>
2021-04-22 13:10:19 +0300

    added json

--- src/script.js
+++ src/script.js
@@ -15,6 +15,12 @@ function run() {
     var resetBtn = document.getElementById("reset");
     resetBtn.addEventListener("click", reset);
 
+    var saveBtn = document.getElementById("save");
+    saveBtn.addEventListener("click", save);
+
+    var restoreBtn = document.getElementById("restore");
+    restoreBtn.addEventListener("click", restore);
+
 
     function increment() {
         textBox.innerHTML = Number(textBox.innerHTML) + 1;
@@ -26,5 +32,20 @@ function run() {
 
     function reset() {
         textBox.innerHTML = 0;
+    }
+
+    function save() {
+        var myNumber = {number: Number(textBox.innerHTML)};
+        var myNuberJson = JSON.stringify(myNumber);
+        localStorage.setItem("numberJson", myNuberJson);
+    }
+
+    function restore() {
+
+
+        var myNuberJson = localStorage.getItem("numberJson");
+        var myNumber = JSON.parse(myNuberJson);
+
+        textBox.innerHTML = myNumber.number;
     }
 }
